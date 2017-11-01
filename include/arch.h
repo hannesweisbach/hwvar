fb04bb66f0a106c7b63a2080b5179a503991d6cc
Hannes Weisbach <hannes.weisbach@riken.jp>
2017-11-01 11:31:29 +0900

    Do arch-dependent time-measurement header-only

--- include/arch.h
+++ include/arch.h
@@ -18,6 +18,7 @@ static inline uint64_t arch_timestamp_begin(void) { return timestamp(); }
 static inline uint64_t arch_timestamp_end(void) { return timestamp(); }
 
 #elif defined(__sparc)
+
 static inline uint64_t timestamp() {
   uint64_t value;
   __asm__ volatile("rd %%tick %0\n" : "=r"(value));
@@ -27,6 +28,31 @@ static inline uint64_t timestamp() {
 static inline uint64_t arch_timestamp_begin(void) { return timestamp(); }
 static inline uint64_t arch_timestamp_end(void) { return timestamp(); }
 
+#elif defined(__x86_64__)
+
+static inline uint64_t arch_timestamp_begin(void) {
+  unsigned high, low;
+  __asm__ volatile("CPUID\n\t"
+                   "RDTSC\n\t"
+                   "mov %%edx, %0\n\t"
+                   "mov %%eax, %1\n\t"
+                   : "=r"(high), "=r"(low)::"%rax", "%rbx", "%rcx", "%rdx");
+
+  return (uint64_t)high << 32ULL | low;
+}
+
+static inline uint64_t arch_timestamp_end(void) {
+  unsigned high, low;
+  __asm__ volatile("RDTSCP\n\t"
+                   "mov %%edx,%0\n\t"
+                   "mov %%eax,%1\n\t"
+                   "CPUID\n\t"
+                   : "=r"(high), "=r"(low)::"%rax", "%rbx", "%rcx", "%rdx");
+  return (uint64_t)high << 32ULL | low;
+}
+
 #else
+
 #error "Unkown/Unsupported architecture"
+
 #endif
