bb4e4f9457a576dc52e93f09c23b9481a0629c1e
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2018-08-17 08:35:32 +0200

    Use rdmsr/wrmsr from policies

--- src/arch/x86_64.h
+++ src/arch/x86_64.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <config.h>
+
 #ifdef JEVENTS_FOUND
 #include <iostream>
 #include <memory>
@@ -66,56 +68,6 @@ static uint64_t rdpmc(const uint32_t counter) {
   return (uint64_t)high << 32 | low;
 }
 
-static uint64_t rdmsr(const uint32_t idx, const int fd) {
-  if (mck_is_mckernel()) {
-    return syscall(850, idx);
-  } else {
-    uint64_t v;
-    const ssize_t ret = pread(fd, &v, sizeof(v), idx);
-    if (ret != sizeof(v)) {
-      printf("Reading MSR failed\n");
-    }
-    return v;
-  }
-}
-
-static uint64_t wrmsr(const uint32_t idx, const uint64_t val, const int fd) {
-  if (mck_is_mckernel()) {
-    return syscall(851, idx, val);
-  } else {
-    const ssize_t ret = pwrite(fd, &val, sizeof(val), idx);
-    if (ret != sizeof(val)) {
-      printf("Writing MSR %x failed\n", idx);
-    }
-    return ret;
-  }
-}
-
-static uint64_t rdmsr(const uint32_t idx, const std::unique_ptr<fd> &fd) {
-  if (mck_is_mckernel()) {
-    return syscall(850, idx);
-  } else {
-    uint64_t v;
-    const ssize_t ret = pread(static_cast<int>(*fd), &v, sizeof(v), idx);
-    if (ret != sizeof(v)) {
-      printf("Reading MSR failed\n");
-    }
-    return v;
-  }
-}
-
-static uint64_t wrmsr(const uint32_t idx, const uint64_t val, const std::unique_ptr<fd>& fd) {
-  if (mck_is_mckernel()) {
-    return syscall(851, idx, val);
-  } else {
-    const ssize_t ret = pwrite(static_cast<int>(*fd), &val, sizeof(val), idx);
-    if (ret != sizeof(val)) {
-      printf("Writing MSR %x failed\n", idx);
-    }
-    return ret;
-  }
-}
-
 enum msrs {
   IA32_PMC_BASE = 0x0c1,
   IA32_PERFEVTSEL_BASE = 0x186,
@@ -134,18 +86,8 @@ static inline void cpuid(const int code, uint32_t *a, uint32_t *b, uint32_t *c,
 
 #define MASK(v, high, low) ((v >> low) & ((1 << (high - low + 1)) - 1))
 
-#if 0
-struct pmu {
-  struct rdpmc_ctx *ctx;
-  int *perf_fds;
-  unsigned active;
-  int mckernel;
-  char *msr;
-  int msr_fd;
-  uint64_t global_ctrl;
-};
-#endif
-static void pmu_info(const std::unique_ptr<fd> &msr_fd) {
+template <typename POLICY>
+static void pmu_info(const POLICY &msr) {
   uint32_t eax, ebx, ecx, edx;
 
   cpuid(0x1, &eax, &ebx, &ecx, &edx);
@@ -158,14 +100,14 @@ static void pmu_info(const std::unique_ptr<fd> &msr_fd) {
   std::cout << "EDX: " << edx << std::endl;
 
   if (ecx & (1 << 15)) {
-    const uint64_t caps = rdmsr(IA32_PERF_CAPABILITIES, msr_fd);
+    const uint64_t caps = msr.rdmsr(IA32_PERF_CAPABILITIES);
     const int vmm_freeze = MASK(caps, 12, 12);
     std::cout << "Caps: " << caps << std::endl;
     std::cout << "VMM Freeze: " << std::boolalpha
               << static_cast<bool>(vmm_freeze) << std::noboolalpha << std::endl;
     if (vmm_freeze) {
-      const uint64_t debugctl = rdmsr(IA32_DEBUGCTL, msr_fd);
-      wrmsr(IA32_DEBUGCTL, debugctl & ~(1 << 14), msr_fd);
+      const uint64_t debugctl = msr.rdmsr(IA32_DEBUGCTL);
+      msr.wrmsr(IA32_DEBUGCTL, debugctl & ~(1 << 14));
     }
   }
 
@@ -191,25 +133,6 @@ static void pmu_info(const std::unique_ptr<fd> &msr_fd) {
 
   std::cout << "FFPCs: " << ffpc << ", width: " << ff_width << std::endl;
 }
-
-#if 0
-class events {
-  std::vector<event> events_;
-
-public:
-  events(const pmc &pmcs) {
-    for (const auto &pmc : pmcs) {
-      try {
-        events_.emplace_back(pmc.name());
-      } catch (...) {
-      }
-    }
-  }
-  auto begin() const { return events_.begin(); }
-  auto end() const { return events_.end(); }
-  auto size() const { return events_.size(); }
-};
-#endif
 
 struct x86_pmc_base {
   virtual ~x86_pmc_base() = default;
@@ -271,7 +194,7 @@ public:
       : fd_(std::make_unique<fd>(open(
             ("/dev/cpu/" + std::to_string(cpu()) + "/msr").c_str(), O_RDWR))) {}
 
-  uint64_t rdmsr(const uint32_t reg) {
+  uint64_t rdmsr(const uint32_t reg) const {
     uint64_t v;
     const ssize_t ret = pread(static_cast<int>(*fd_), &v, sizeof(v), reg);
     if (ret != sizeof(v)) {
@@ -281,7 +204,7 @@ public:
     return v;
   }
 
-  uint64_t wrmsr(const uint32_t reg, const uint64_t val) {
+  uint64_t wrmsr(const uint32_t reg, const uint64_t val) const {
     const ssize_t ret = pwrite(static_cast<int>(*fd_), &val, sizeof(val), reg);
     if (ret != sizeof(val)) {
       std::cerr << "Writing MSR " << std::hex << reg << std::dec << " failed."
@@ -291,42 +214,45 @@ public:
   }
 };
 
-template <typename MSR>
-class rawmsr_policy : MSR {
+template <typename MSR> class rawmsr_policy : MSR {
+  uint64_t global_ctrl_;
+
 public:
+  rawmsr_policy() : global_ctrl_(this->rdmsr(IA32_PERF_GLOBAL_CTRL)) {
+    pmu_info(static_cast<MSR&>(*this));
+  }
+  ~rawmsr_policy() { this->wrmsr(IA32_PERF_GLOBAL_CTRL, global_ctrl_); }
   int init(const int i, const int config) {
     const uint64_t v = (1 << 22) | (1 << 16) | config;
-    wrmsr(IA32_PERFEVTSEL_BASE + i, v, {});
+    this->wrmsr(IA32_PERFEVTSEL_BASE + i, v);
     return 0;
   }
 
-  void reset(const int i) { wrmsr(IA32_PMC_BASE + i, 0, {}); }
+  void reset(const int i) { this->wrmsr(IA32_PMC_BASE + i, 0); }
   void start(const unsigned long mask) {
-    wrmsr(IA32_PERF_GLOBAL_OVF_CTRL, 0, {});
-    wrmsr(IA32_PERF_GLOBAL_CTRL, mask, {});
+    this->wrmsr(IA32_PERF_GLOBAL_OVF_CTRL, 0);
+    this->wrmsr(IA32_PERF_GLOBAL_CTRL, mask);
   }
   void stop(const unsigned long mask) {
-    wrmsr(IA32_PERF_GLOBAL_CTRL, 0, {});
-    const uint64_t ovf = rdmsr(IA32_PERF_GLOBAL_STATUS, {});
+    this->wrmsr(IA32_PERF_GLOBAL_CTRL, 0);
+    const uint64_t ovf = this->rdmsr(IA32_PERF_GLOBAL_STATUS);
     if (ovf) {
       std::cout << "Overflow: " << std::hex << ovf << std::dec << std::endl;
-      wrmsr(IA32_PERF_GLOBAL_OVF_CTRL, 0, {});
+      this->wrmsr(IA32_PERF_GLOBAL_OVF_CTRL, 0);
     }
   }
   /* Should be equivalent to rdpmc instruction */
-  uint64_t read(const int i) { return rdmsr(IA32_PMC_BASE + i, {}); }
+  uint64_t read(const int i) { return this->rdmsr(IA32_PMC_BASE + i); }
 };
 
 template <typename ACCESS>
 class msr_pmc : ACCESS, public x86_pmc_base {
-  uint64_t global_ctrl_;
   int active = 0;
   unsigned long active_mask_ = 0;
 public:
   /* sufficient to pass vec of perf_event_attr */
   msr_pmc(const pmc &pmcs)
-      : global_ctrl_(rdmsr(IA32_PERF_GLOBAL_CTRL, -1)) {
-    pmu_info({});
+       {
     for (const auto &pmc : pmcs) {
       const int err = ACCESS::init(active, pmc.data().attr().config);
       if (err) {
@@ -340,7 +266,6 @@ public:
 
     active_mask_ = static_cast<unsigned long>((1 << active) - 1);
   }
-  ~msr_pmc() { wrmsr(IA32_PERF_GLOBAL_CTRL, global_ctrl_, {}); }
 
   void start(gsl::span<uint64_t>::iterator) override {
     for (int i = 0; i < active; ++i) {
